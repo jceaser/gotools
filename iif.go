aab92103342bada62cf4ef98288261bdad0f3599
Thomas Cherry <thomas.a.cherry@nasa.gov>
2018-10-19 17:11:39 -0400

    fixing the tests

--- iif.go
+++ iif.go
@@ -1,77 +1,53 @@
 package main
 
-import ("fmt"
+import ("os"
+	"fmt"
     "flag"
+	"strconv"
     )
 
 /*
 iff left cmd right value other
 */
 func main() {
-    //args := os.Args
-    
-    left := flag.String("left", "true", "line mode, trim each line")
-    right := flag.String("right", "true", "edge mode, trim just edges")
-    //allMode := flag.String("all", false, "all mode, trim everything")
+    left := flag.String("left", "true", "left hand")
+	cmd := flag.String("test", "==", "actions: [== != <= < >= > && ||]")
+	right := flag.String("right", "true", "right hand")
+	value := flag.String("value", "true", "return value on success")
+	alt := flag.String("alt", "false", "return value on fail")
     
     flag.Parse()
     
-    //fmt.Printf("%s\n%s\ndone\n", *left, *right)
-    
-    /*for _, a := range flag.Args() {
-        fmt.Printf("'%s'\n", a)
-    }*/
-    
     state := "unknown"
-    var cmd string
-    var value string
-    var alt string
     
-    if 0<len(flag.Args()){
-        *left = flag.Arg(0)
-    }
-    
-    if 1<len(flag.Args()){
-        cmd = flag.Arg(1)
-    } else {
-        cmd = "=="
-    }
-    if 2<len(flag.Args()){
-        *right = flag.Arg(2)
-    } else {
-        *right = "true"
-    }
-    
-    if 3<len(flag.Args()){
-        value = flag.Arg(3)
-    } else {
-        value = "true"
-    }
-    
-    if 4<len(flag.Args()){
-        alt = flag.Arg(4)
-    } else {
-        alt = "false"
-    }
-    
-    /*****/
-    
-    switch cmd {
+    switch *cmd {
         case "==":
-            state = iff (*left==*right, value, alt)
+            state = iff (*left==*right, *value, *alt)
         case "!=":
-            state = iff (*left!=*right, value, alt)
+            state = iff (*left!=*right, *value, *alt)
         case "<=":
-            state = iff (*left<=*right, value, alt)
+            state = iff (*left<=*right, *value, *alt)
         case "<":
-            state = iff (*left<*right, value, alt)
+            state = iff (*left<*right, *value, *alt)
         case ">=":
-            state = iff (*left>=*right, value, alt)
+            state = iff (*left>=*right, *value, *alt)
         case ">":
-            state = iff (*left>*right, value, alt)
+            state = iff (*left>*right, *value, *alt)
+		case "&&":
+			l, _ := strconv.ParseBool(*left)
+			r, _ := strconv.ParseBool(*right)
+			state = iff (l && r, *value, *alt)
+		case "||":
+			l, _ := strconv.ParseBool(*left)
+			r, _ := strconv.ParseBool(*right)
+			state = iff (l || r, *value, *alt)
     }
     fmt.Printf("%s\n", state)
-
+	if (state==*value) {
+		os.Exit(0);
+	} else {
+		os.Exit(1);	
+	}
 }
 
 func iff(test bool, good string, bad string) string {

8a16c2469e34bdab9bdfacd1adcf73be3a82bd53
Thomas Cherry <thomas.cherry@gmail.com>
2019-04-14 19:35:30 -0400

    added interactive mode and some new functions

--- rpn.go
+++ rpn.go
@@ -3,18 +3,41 @@ package main
 import ("fmt"
     "bufio"
     "os"
-    "io"
-    "bytes"
+    //"io"
+    //"bytes"
     "flag"
     "math"
+    "math/rand"
+    "sort"
     "strconv"
     "syscall"
     "unsafe"
     "strings"
-    "os/exec"
+    //"os/exec"
+    "path/filepath"
+    "github.com/peterh/liner"
     )
 
 /****/
+
+type func_data struct {
+    cmd func()
+    doc string
+}
+
+var (
+    history_fn = filepath.Join(os.TempDir(), ".rpn_history")
+    names      = []string{"print", "dump", "quit"}
+
+    stack []float64
+    mainstack []float64
+    altstack []float64
+
+    memory = make(map[string]float64)
+
+    actions = make(map[string]func_data)
+)
+
 type winsize struct {
     Row    uint16
     Col    uint16
@@ -34,47 +57,97 @@ func getWidth() uint {
     }
     return uint(ws.Col)
 }
+
 /****/
 
+func setup_liner(line *liner.State) {
+    line.SetCtrlCAborts(true)
 
-/**
-pull each line and process
-*/
-func ReadLines(reader io.Reader, foo func(string) string) string {
-    buf := bufio.NewReader(reader)
-    var buffer bytes.Buffer
-    line, err := buf.ReadBytes('\n');
-    for err == nil {
-        if foo!=nil {
-            line = []byte(foo(string(line)))
+    line.SetTabCompletionStyle(liner.TabPrints)
+    line.SetCompleter(func(line string) (c []string) {
+        for _, n := range names {
+            fmt.Print(n)
+            if strings.HasPrefix(n, strings.ToLower(line)) {
+                c = append(c, n)
+                fmt.Print(n)
+            }
         }
-        buffer.Write(line)
-        line, err = buf.ReadBytes('\n')
+        return
+    })
+    if f, err := os.Open(history_fn); err == nil {
+        line.ReadHistory(f)
+        f.Close()
     }
-    return buffer.String()
 }
 
-var stack []float64
-var mainstack []float64
-var altstack []float64
+func a(key string, foo func(), doc string) {
+    actions[key] = func_data{cmd:foo, doc:doc}
+}
 
-var memory = make(map[string]float64)
+func InitializeActions() {
+    // unary actions
+    a("drop", Drop, "Remove the top item from the stack")
+    a("--", Decrement, "subtract one from the top of the stack")
+    a("++", Increment, "add one to the top of the stack")
+    a("^2", Square, "square the item at the top of the stack")
+    a("rand", Random, "Generates a random number from 0-1")
+    
+    // binary actions
+    a("+", Plus, "add two numbers")
+    a("-", Minus, "subtract two numbers")
+    a("*", Times, "multiply two numbers")
+    a("/", Divide, "divide two numbers")
+    a("%", Remainder, "divide two numbers return only the remainder")
+    a("^", Power, "take the power of two numbers first^second")
+    a("min", Min, "return the smaller of two numbers")
+    a("max", Max, "return the larger of two numbers")
+    a("<>", Swap, "swap the top two stack items")
+
+    // ternary actions
+    a("?<", IfLess, "if s[2]<0 then s[1] else s[0], consumes all three")
+    a("?>", IfOver, "if s[2]>0 then s[1] else s[0], consumes all three")
+
+    // Whole stack actions
+    a("<<", RotateLeft, "shift stack down, wrapping")
+    a(">>", RotateRight, "shift stack up, wrapping")
+    a("avg", Average, "Take the average of the entire stack")
+    a("stddev", StandardDeviation, "Take the standard deviation of the stack")
+    a("sort", Sort, "")
+    a("med", Median, "")
+    a("clear", Clear, "Empty the stack")
+
+    //other actions
+    a("swap", SwapStacks, "not implimented yet")
+    a("quit", Exit, "Quit application, same as exit")
+    a("exit", Exit, "Quit application, same as quit")
+    a("help", Help, "Display a help document")
+    a("print", Print, "print the stack")
+    a("dump", Dump, "print out memory storage")
+}
 
 func main() {
-    //args := os.Args
-    
-    formula := flag.String("formula", "print", "math formula in RPN format")
-    useStream := flag.Bool("stream", false, "use input stream")
-    interactive := flag.Bool("interactive", false, "interactive mode")
+    InitializeActions()
+
+    //readline setup
+    line := liner.NewLiner()
+    defer line.Close()
+    setup_liner(line)
+
+    //flag setup
+    formula := flag.String("formula", "print",
+        "math formula in RPN format, interpreted before stream")
+    interactive := flag.Bool("interactive", false,
+        "interactive mode using a readline style interface")
     verbose := flag.Bool("verbose", false, "verbose")
     
     flag.Parse()
-    
+
+    //process stream if it exists
     stat, _ := os.Stdin.Stat()
-    if *useStream || ( (stat.Mode() & os.ModeCharDevice) == 0 ) {
+    if  ( (stat.Mode() & os.ModeCharDevice) == 0 ) {
         scanner := bufio.NewScanner(os.Stdin)
         for scanner.Scan() {
-            a := fmt.Sprintf("%s %s", scanner.Text(), *formula)
+            a := fmt.Sprintf("%s %s", *formula, scanner.Text())
             formula = &a
         }
     }
@@ -82,35 +155,54 @@ func main() {
     //stack = &mainstack
 
     if *interactive {
-
-        exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-        exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
-        //var b []byte = make([]byte, 1)
-
-        fmt.Print("Enter text: ")
-        for {
-            fmt.Printf(">")
-            var input string
-            input = ReadFormula()
-            //fmt.Scanf("%q", &input)
-            //os.Stdin.Read(b)
-            //fmt.Printf(string(b))
-            //scanner := bufio.NewScanner(os.Stdin)
-            //if scanner.Scan() {input = scanner.Text()}
-
-            //fmt.Printf("\n%s\n", input)
-            //fmt.Printf("\n")
-            
-            
-
-            input = strings.Trim(input, " ")
-            ProcessLine(input, *verbose)
-        }
+        InteractiveAdvance(line, verbose)
+        //InteractiveBasic()
     } else  {
         ProcessLine(*formula, *verbose)
     }
 }
 
+/**
+run the interactive mode using the third party readline library. Help the 
+library stor history, take each line and send it to ProcessLine()
+*/
+func InteractiveAdvance(line *liner.State, verbose *bool) {
+    for {
+        if name, err := line.Prompt(">"); err == nil {
+            input := strings.Trim(name, " ")    //clean it
+            line.AppendHistory(name)            //save it
+            ProcessLine(input, *verbose)        //use it
+        } else if err == liner.ErrPromptAborted {
+            fmt.Print("Aborted")
+        } else {
+            fmt.Print("Error reading line: ", err)
+        }
+        //save the history
+        if f, err := os.Create(history_fn); err != nil {
+            fmt.Print("Error creating history file: ", err)
+        } else {
+            line.WriteHistory(f)
+            f.Close()
+        }
+    }
+}
+
+/**
+this is a fall back method in case it is decided to not use a third party 
+interface
+*/
+func InteractiveBasic(verbose *bool) {
+    fmt.Print("Enter text: ")
+    for {
+        fmt.Printf(">")
+        var input string
+        input = ReadFormula()
+        input = strings.Trim(input, " ")
+        ProcessLine(input, *verbose)
+    }
+}
+
+/** used */
 func ReadFormula() string {
     /*var b []byte = make([]byte, 1)*/
 
@@ -119,15 +211,13 @@ func ReadFormula() string {
         fmt.Printf(">")
         var input string
         var ascii int
-        //var keyCode int
-        //var err error
         getChar(ascii /*, keyCode, err*/)
-        //fmt.Scanf("%q", &input)
         fmt.Printf(string(ascii))
         fmt.Printf(input)
     }
 }
 
+/** used */
 func getChar(ascii int) {
     reader := bufio.NewReader(os.Stdin)
     // ...
@@ -138,58 +228,9 @@ func getChar(ascii int) {
     }
 }
 
-func _getChar(ascii int) {
-    exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-    exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
-    defer exec.Command("stty", "-F", "/dev/tty", "echo").Run()
-    
-    var b []byte = make([]byte, 1)
-    for {
-        os.Stdin.Read(b)
-        fmt.Println("got the byte ", b, " ("+string(b)+")")
-        ascii = 20
-    }
-}
-/*
-func getChar(ascii int, keyCode int, err error) {
-	t, _ := term.Open("/dev/tty")
-	term.RawMode(t)
-	bytes := make([]byte, 3)
-
-	var numRead int
-	numRead, err = t.Read(bytes)
-	if err != nil {
-		return
-	}
-	if numRead == 3 && bytes[0] == 27 && bytes[1] == 91 {
-		// Three-character control sequence, beginning with "ESC-[".
-
-		// Since there are no ASCII codes for arrow keys, we use
-		// Javascript key codes.
-		if bytes[2] == 65 {
-			// Up
-			keyCode = 38
-		} else if bytes[2] == 66 {
-			// Down
-			keyCode = 40
-		} else if bytes[2] == 67 {
-			// Right
-			keyCode = 39
-		} else if bytes[2] == 68 {
-			// Left
-			keyCode = 37
-		}
-	} else if numRead == 1 {
-		ascii = int(bytes[0])
-	} else {
-		// Two characters read??
-	}
-	t.Restore()
-	t.Close()
-	return
-}
+/**
+process a formula line
 */
-
 func ProcessLine(formula string, verbose bool) {
     for _, segment:= range strings.Split(formula, " ") {
         cmd := segment
@@ -213,53 +254,47 @@ func ProcessLine(formula string, verbose bool) {
     }
 }
 
+/**
+Perform a single action/operation. Either store a value or operate on it
+*/
 func Action (segment string, verbose bool) {
     value, err := strconv.ParseFloat(segment, 64)
     if err==nil {
         Push(value)
     } else {
+        foo := actions[segment].cmd
+        doc := actions[segment].doc
+
+        if verbose && len(doc)>0 {
+            fmt.Printf("%s\n", doc)
+        }
+
+        if foo!=nil {
+            foo()
+        } else {
         switch segment {
-            case "quit": os.Exit(0)
-
-            case "+": Plus()
-            case "-": Minus()
-            case "*": Times()
-            case "/": Divide()
-            case "%": Remainder()
-            case "^": Power()
-            case "min": Min()
-            case "max": Max()
-
-            case "rand":
-            case "--": Decrement()
-            case "++": Increment()
-            case "^2": Square()
-            case "print": Print()
-            case "<>": Swap()
-            case "<<": RotateLeft()
-            case ">>": RotateRight()
-            case "swap": SwapStacks()
-
-            case "?<": IfLess()
-            case "?>": IfOver()
-
             case "a","b","c","d","e","f","g","h","i","j","k","l","m",
                     "n","o","p","q","r","s","t","u","v","w","x","y","z":
                 MemoryLoad(segment)
             case "A","B","C","D","E","F","G","H","I","J","K","L","M",
                     "N","O","P","Q","R","S","T","U","V","W","X","Y","Z":
                 MemoryStore( strings.ToLower(segment) )
-            case "dump": Dump()
-
             default:
-                fmt.Printf("%s is an unknown command", segment)
+                fmt.Printf("%s is an unknown command\n", segment)
+        }
         }
     }
+
     if verbose {
         fmt.Printf("%v\n", stack)
     }
 
 }
+
+/******************************************************************************/
+// #mark - General Operations
+
+// memory functions
 
 func MemoryLoad (key string) {//recall value
     var value = memory[key]     
@@ -274,12 +309,27 @@ func Dump() {
     fmt.Printf("(%v)\n", memory);
 }
 
+// stack functions
+
+func Items() bool {
+    return len(stack)>0
+}
+
+func Empty() bool {
+    return len(stack)<1
+}
+
 func Push(value float64) {
     stack = append(stack, value)
 }
 
 func Pop() float64 {
-    n := len(stack) - 1
+    l := len(stack)
+    if l < 1 {
+        fmt.Printf("stack is empty %d\n", len(stack))
+        return 0.0
+    }
+    n := l - 1
     value := 0.0
     value, stack = stack[n], stack[:n]
     return value
@@ -296,12 +346,51 @@ func PopQueue() float64 {
     return value
 }
 
+// print functions
 func Print() {
     fmt.Printf("(%v)\n", stack)
 }
 
-/**/
-/* trinaries */
+func Help() {
+    fmt.Printf("\n%s\n\n", strings.Repeat("*", 80) )
+    
+f := "%15s : %-8s %s\n"
+    fmt.Printf(f, "Flag", "Category", "Description")
+    fmt.Printf(f, "----", "--------", "-----------")
+    fmt.Printf(f, "--formula", "Input", "Accept formulas from standard in")
+    fmt.Printf(f, "--help", "help", "Explanation of parameters")
+    fmt.Printf(f, "--interactive", "Mode", "use a readline like interface")
+    fmt.Printf(f, "--stream", "Input", "Accept formulas from standard in")
+    fmt.Printf(f, "--verbose", "output",
+        "output more details about inter workings")
+
+    fmt.Printf("\n%s\n\n", strings.Repeat("*", 80) )
+    
+    keys := make( []string, 0 )
+    for k := range actions {
+        keys = append(keys, k)
+    }
+    sort.Strings(keys)
+
+    for _, k := range keys {
+        doc := actions[k].doc
+        fmt.Printf("%8s = %s\n", k, doc)
+    }
+    fmt.Printf("\n%s\n\n", strings.Repeat("*", 80) )
+
+    fmt.Printf("%s\n", "a~z will store values (push)")
+    fmt.Printf("%s\n", "A~Z will recall values (pop)")
+    fmt.Printf("%s\n", "action:count will call action 'count' times : `+:3`")
+}
+
+// system functions 
+
+func Exit(){
+    os.Exit(0)
+}
+
+/**************************************/
+// #mark - ternary operators
 
 func IfLess() {
     fail := Pop()
@@ -325,8 +414,8 @@ func IfOver() {
     }
 }
 
-/**/
-/* Doubles */
+/**************************************/
+// #mark - binary operators
 
 func Plus() {
     right := Pop()
@@ -377,8 +466,8 @@ func Swap() {
     Push( left )
 }
 
-/*****/
-/* singletons */
+/**************************************/
+// #mark - unary operators
 
 func SwapStacks() {
     /*if &stack == &mainstack {
@@ -388,6 +477,10 @@ func SwapStacks() {
         fmt.Printf("switch to main %p\n", stack);
         stack = mainstack
     }*/
+}
+
+func Drop() {
+    Pop()
 }
 
 func SquareRoot() {
@@ -406,19 +499,6 @@ func Round() {
     Push( math.Round(top) )
 }
 
-func RotateLeft() {
-    //  <<  take off end and put at beginning
-    //value := stack[0]
-    //stack = stack[1:]
-    value := PopQueue()
-    Push(value)
-}
-func RotateRight() {
-    //  >>  take off beginning and put at end
-    value := Pop()
-    stack = append([]float64{value}, stack...)
-}
-
 func Increment() {
     value := Pop()
     Push(value + 1)
@@ -427,6 +507,89 @@ func Increment() {
 func Decrement() {
     value := Pop()
     Push(value - 1)
+}
+
+func Random() {
+    Push(rand.Float64())
+}
+
+/**************************************/
+/* full stack operators */
+
+/** clear the stack ; empty the stack */
+func Clear() {
+    stack = []float64{}
+}
+
+/** Rotate the stack by taking off the end and putting at the beginning */
+func RotateLeft() {
+    //  <<  take off end and put at beginning
+    value := PopQueue()
+    Push(value)
+}
+
+/** Rotate the stack by taking off beginning and putting at the end */
+func RotateRight() {
+    //  >>  take off beginning and put at end
+    value := Pop()
+    stack = append([]float64{value}, stack...)
+}
+
+/** Average the entire stack */
+func Average() {
+    var total = 0.0;
+    var count = 0.0
+    for Items() {
+        total = total + Pop()
+        count = count + 1
+    }
+    ans := total / count
+    Push(ans)
+}
+
+/**
+>3 5 9 1 8 6 58 9 4 10 stddev print
+([15.8117045254457])
+*/
+func StandardDeviation() {
+    var sum, mean, sd float64 = 0, 0, 0
+    count_i := len(stack)
+    count_f := float64(count_i)
+
+    for i:=0 ; i<count_i; i++ {
+        sum += stack[i]
+    }
+    mean = sum / count_f
+    for i:=0 ; i<count_i; i++ {
+        sd += math.Pow( stack[i]-mean, 2)
+    }
+    sd = math.Sqrt( sd / count_f)
+    Clear()
+    Push(sd)
+}
+
+func Sort() {
+    sort.Float64s(stack)
+}
+
+/**
+>3 5 9 1 8 6 58 9 4 10 med print
+([7])
+>clear
+>3 5 9 1 8 6 58 9 4 med print
+([6])
+*/
+func Median() {
+    Sort()
+    med := 0.0
+    count := len(stack)/2
+    if count % 2 == 0 {//odd use case
+        med = stack[count]
+    } else {//even use case
+        med = ( stack[count] + stack[count-1] ) / 2
+    }
+    Clear()
+    Push(med)
 }
 
 func Filter (line string) string{
